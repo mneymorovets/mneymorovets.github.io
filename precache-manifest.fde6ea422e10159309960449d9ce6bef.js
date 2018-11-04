02a8f77575ae2250e8c41cd93cdeb80319f83482
mneymorovets <mari4kaneimorovest@gmail.com>
2018-11-04 15:36:00 +0200

    Initial commit

---
+++ precache-manifest.fde6ea422e10159309960449d9ce6bef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9736541a6e0cae836b1134caa54d3051",
+    "url": "/mashacv/static/media/image.9736541a.jpg"
+  },
+  {
+    "revision": "30edd90468bd19546025",
+    "url": "/mashacv/static/js/runtime~main.30edd904.js"
+  },
+  {
+    "revision": "1d31660de08e5e0b9669",
+    "url": "/mashacv/static/js/main.1d31660d.chunk.js"
+  },
+  {
+    "revision": "3eb2b9e11dc2b4ca7651",
+    "url": "/mashacv/static/js/1.3eb2b9e1.chunk.js"
+  },
+  {
+    "revision": "1d31660de08e5e0b9669",
+    "url": "/mashacv/static/css/main.cafeda49.chunk.css"
+  },
+  {
+    "revision": "3eb2b9e11dc2b4ca7651",
+    "url": "/mashacv/static/css/1.d8cd5f59.chunk.css"
+  },
+  {
+    "revision": "af606a96ce69f63ba6b1af4a40cc6782",
+    "url": "/mashacv/index.html"
+  }
+];
